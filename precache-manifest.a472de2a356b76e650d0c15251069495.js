310088cffe71abe8fc40b56e95f36fee746bfaab
khiemdc <khiemle.dc@gmail.com>
2020-08-09 17:02:11 -0400

    Updates

---
+++ precache-manifest.a472de2a356b76e650d0c15251069495.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21cd7bf5777d68a84473b15061efd905",
+    "url": "/Tingting-multitasking/index.html"
+  },
+  {
+    "revision": "a90b050c1d9c6b7daadc",
+    "url": "/Tingting-multitasking/static/css/main.e23e6b48.chunk.css"
+  },
+  {
+    "revision": "9e8ef5661b5779c22c45",
+    "url": "/Tingting-multitasking/static/js/2.5e6e0aec.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/Tingting-multitasking/static/js/2.5e6e0aec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a90b050c1d9c6b7daadc",
+    "url": "/Tingting-multitasking/static/js/main.3e79e2e8.chunk.js"
+  },
+  {
+    "revision": "5694f2a1951fd70dc8bb",
+    "url": "/Tingting-multitasking/static/js/runtime-main.f5f9e834.js"
+  }
+]);
